9da20caba2700cc9d19b9f810a4e748cb5fc4336
tzaibach <josec.ramirez@gmail.com>
2022-01-04 12:00:15 +0100

    añadir FreeFileSync 11.16

---
+++ freefilesync@11.rb
@@ -0,0 +1,17 @@
+cask 'freefilesync' do
+  version '11.16'
+  sha256 '9455f8236dab4850b8c52e8278cf23c08a53982baf33fe8391be63ce8503453b'
+
+  url "https://freefilesync.org/download/FreeFileSync_#{version}_macOS.zip",
+      user_agent: :fake,
+      referer:    'https://freefilesync.org/download.php'
+  name 'FreeFileSync'
+  homepage 'https://www.freefilesync.org/'
+
+  pkg "FreeFileSync_#{version}_macOS.pkg"
+
+  uninstall pkgutil: [
+                       'org.freefilesync.pkg.FreeFileSync',
+                       'org.freefilesync.pkg.RealTimeSync',
+                     ]
+end
