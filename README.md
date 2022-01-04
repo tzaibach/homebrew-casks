d45084158cec7e90aa7212cab22eacfbd7ea5697
tzaibach <josec.ramirez@gmail.com>
2022-01-04 11:32:48 +0100

    añadir Double Commander Cocoa 1.0.0-9750

---
+++ README.md
@@ -0,0 +1,11 @@
+# Queco Casks
+
+## How do I install these formulae?
+
+`brew install tzaibach/casks/<formula>`
+
+Or `brew tap tzaibach/casks` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
---
+++ double-commander-cocoa.rb
@@ -0,0 +1,10 @@
+cask "double-commander-cocoa" do
+  version "1.0.0-9750"
+  sha256 "a035031b505ceee4ace49142a7a6a6dcf2d0dd8716c9183eb86b06f38dedbae9"
+
+  url "https://doublecmd.sourceforge.io/snapshots/doublecmd-#{version}.cocoa.x86_64.zip"
+  name "Double Commander"
+  homepage "https://doublecmd.sourceforge.io/"
+
+  app "Double Commander.app"
+end
