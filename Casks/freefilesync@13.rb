1a49dbc62376352f1cfe3ec74ace6ee4939e78d1
tzaibach <josec.ramirez@gmail.com>
2024-05-30 20:05:14 +0200

    upgrade freefilesync@13.rb

--- Casks/freefilesync@13.rb
+++ Casks/freefilesync@13.rb
@@ -1,12 +1,14 @@
-cask 'freefilesync@11' do
-  version '11.28'
-  sha256 'fd6ba57a380e87ab728cdd8794d4efe165a44c914e7e0b99e224186b6b10900c'
+cask 'freefilesync@13' do
+  version '13.6'
+  sha256 :no_check
 
   url "https://freefilesync.org/download/FreeFileSync_#{version}_macOS.zip",
       user_agent: :fake,
       referer:    'https://freefilesync.org/download.php'
   name 'FreeFileSync'
   homepage 'https://www.freefilesync.org/'
+
+  auto_updates true
 
   pkg "FreeFileSync_#{version}.pkg"
 
