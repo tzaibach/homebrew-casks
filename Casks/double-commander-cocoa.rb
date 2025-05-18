494906b50fc1f4337bdec0016e993c5c0c2f653e
tzaibach <josec.ramirez@gmail.com>
2025-05-18 14:04:34 +0200

    downgrade double-commander-cocoa to 1.1.14

--- Casks/double-commander-cocoa.rb
+++ Casks/double-commander-cocoa.rb
@@ -1,6 +1,6 @@
 cask "double-commander-cocoa" do
-  version "1.1.22"
-  sha256 "5761863ef4df21b4d2d5d49dbb675254887cefe656eb97e2e7c97cb29b48764c"
+  version "1.1.14"
+  sha256 "dd00f3c6035294d4a31422379cb00470f609b86cd9c8d449d686d3e9f35f4ecd"
 
   url "https://github.com/doublecmd/doublecmd/releases/download/v#{version}/doublecmd-#{version}.cocoa.x86_64.dmg"
   name "Double Commander"
