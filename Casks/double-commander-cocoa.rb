226180f64fb766c4e6b79ddf5bc5c29a625f0fa6
tzaibach <josec.ramirez@gmail.com>
2025-05-18 13:23:49 +0200

    upgrade double-commander-cocoa to 1.1.24

--- Casks/double-commander-cocoa.rb
+++ Casks/double-commander-cocoa.rb
@@ -1,6 +1,6 @@
 cask "double-commander-cocoa" do
-  version "1.1.0-10014"
-  sha256 "81077e87929b08e63585133552505fc5025eb135eac494c8ad3e018c8fe484e6"
+  version "1.1.24"
+  sha256 "ad0d09f3977664b3693d7461166500395bd4168fe1cea94acdd1fd2d2f0abf6e"
 
   url "https://doublecmd.sourceforge.io/snapshots/doublecmd-#{version}.cocoa.x86_64.zip"
   name "Double Commander"
